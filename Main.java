f623406e7e8fcc7d10a329a76ac1fa3e533ff877
aditxa <109591193+aditxa@users.noreply.github.com>
2024-10-16 00:21:15 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,25 +1,30 @@
 package com.aditya.angrybirdsclone;
 
 import com.badlogic.gdx.Game;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.aditya.angrybirdsclone.screens.HomeScreen;
+import com.aditya.angrybirdsclone.screens.GameScreen;
+import com.aditya.angrybirdsclone.screens.PauseScreen;
 
 public class Main extends Game {
-    public SpriteBatch batch;
 
     @Override
     public void create() {
-        batch = new SpriteBatch();
-        setScreen(new HomeScreen(this));  // Set the home screen as the default screen
+        // Start the game by displaying the Home Screen.
+        this.setScreen(new HomeScreen(this));
     }
 
-    @Override
-    public void render() {
-        super.render();  // Render the active screen
+    // Method to switch to the Game Screen
+    public void startGame() {
+        this.setScreen(new GameScreen(this));
     }
 
-    @Override
-    public void dispose() {
-        batch.dispose();
+    // Method to switch to the Pause Screen
+    public void pauseGame() {
+        this.setScreen(new PauseScreen(this));
+    }
+
+    // Method to return to the Home Screen
+    public void returnToHome() {
+        this.setScreen(new HomeScreen(this));
     }
 }
